6d32f7aaa1f0fdc317bb01a7597415f2cd232a84
Ariel Salcé <arielsalcepk@gmail.com>
2026-08-13 17:02:43 -0400

    Add README, remove generated comments, replace default branding (#6)

--- lib/ecommerce_challenge/application.ex
+++ lib/ecommerce_challenge/application.ex
@@ -1,6 +1,4 @@
 defmodule EcommerceChallenge.Application do
-  # See https://elixir.hexdocs.pm/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
@@ -13,20 +11,13 @@ defmodule EcommerceChallenge.Application do
       {DNSCluster,
        query: Application.get_env(:ecommerce_challenge, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: EcommerceChallenge.PubSub},
-      # Start a worker by calling: EcommerceChallenge.Worker.start_link(arg)
-      # {EcommerceChallenge.Worker, arg},
-      # Start to serve requests, typically the last entry
       EcommerceChallengeWeb.Endpoint
     ]
 
-    # See https://elixir.hexdocs.pm/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: EcommerceChallenge.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   @impl true
   def config_change(changed, _new, removed) do
     EcommerceChallengeWeb.Endpoint.config_change(changed, removed)
--- lib/ecommerce_challenge_web/router.ex
+++ lib/ecommerce_challenge_web/router.ex
@@ -25,18 +25,7 @@ defmodule EcommerceChallengeWeb.Router do
     live "/orders/:id", OrderLive.Show, :show
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", EcommerceChallengeWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:ecommerce_challenge, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
