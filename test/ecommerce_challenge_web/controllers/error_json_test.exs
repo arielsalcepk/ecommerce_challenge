9c2eeaa1a7bc3235d5eec0c4859f231d6a300613
Ariel Salcé <arielsalcepk@gmail.com>
2026-08-13 16:12:45 -0400

    Add missing error view and telemetry modules from initial scaffold (#2)

--- test/ecommerce_challenge_web/controllers/error_json_test.exs
+++ test/ecommerce_challenge_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule EcommerceChallengeWeb.ErrorJSONTest do
   use EcommerceChallengeWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert EcommerceChallengeWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert EcommerceChallengeWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
